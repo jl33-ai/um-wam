6d5abfdcb4a4f1b28551c22286763207de1072ef
Justin <justinkhlee27@gmail.com>
2023-11-24 16:55:34 +1100

    c-tot

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if 'grades' not in st.session_state:
 
 st.sidebar.markdown("### How Your WAM is Calculated")
 st.sidebar.markdown("See the official [University of Melbourne website](https://students.unimelb.edu.au/your-course/manage-your-course/exams-assessments-and-results/results-and-academic-statements/wam#:~:text=It%20is%20calculated%20progressively%20as,subject%20in%20calculating%20your%20WAM.) for more details.")
-st.sidebar.latex(r"\sum_{i=1}^{n} c_i \times g_i")
+st.sidebar.latex(r"\sum_{i=1}^{n} \frac{c_i}{c_{tot}} \times g_i")
 st.sidebar.markdown("Where `n` is the number of subjects you have completed so far, `c_i` is the credit points for subject `i`, and `g_i` is the grade for subject `i`.")
 st.sidebar.markdown("\n\n")
 st.sidebar.markdown('---')
