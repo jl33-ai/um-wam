e63bb3f101174c05fbe1c0f63c1b873251099288
jl33-ai <justinkhlee27@gmail.com>
2025-02-21 09:45:28 +1100

    add menu items

--- src/components/header.py
+++ src/components/header.py
@@ -5,6 +5,10 @@ def render_head():
     st.set_page_config(
         page_title="University WAM Calculator",
         initial_sidebar_state="collapsed",
-        page_icon="🎓"
+        page_icon="🎓",
+        menu_items={
+            'Report a bug': "https://github.com/jl33-ai/um-wam/issues/new",
+            'About': "# Calculate your university weighted average mark (WAM)",
+        }
     )
     st.header("Calculate Weighted Average Mark (WAM)")
